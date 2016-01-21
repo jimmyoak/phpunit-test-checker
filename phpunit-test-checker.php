799fea0018706506b4dcace1d09568892d674c3f
Adrián Robles <adrian.robles@scmspain.com>
2016-01-21 17:05:44 +0100

    bin/phpunit-test-checker.php fix

--- phpunit-test-checker.php
+++ phpunit-test-checker.php
@@ -1,6 +1,26 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+foreach (array(__DIR__ . '/../../autoload.php', __DIR__ . '/../vendor/autoload.php', __DIR__ . '/vendor/autoload.php') as $file) {
+    if (file_exists($file)) {
+        define('PHPUNIT_TEST_CHECKER_COMPOSER_INSTALL', $file);
+
+        break;
+    }
+}
+
+unset($file);
+
+if (!defined('PHPUNIT_TEST_CHECKER_COMPOSER_INSTALL')) {
+    fwrite(STDERR,
+        'You need to set up the project dependencies using the following commands:' . PHP_EOL .
+        'wget http://getcomposer.org/composer.phar' . PHP_EOL .
+        'php composer.phar install' . PHP_EOL
+    );
+
+    die(1);
+}
+
+require_once PHPUNIT_TEST_CHECKER_COMPOSER_INSTALL;
 
 use JimmyOak\PhpUnitChecker\Command\CheckerCommand;
 use Symfony\Component\Console\Input\ArgvInput;
